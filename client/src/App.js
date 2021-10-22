de385aa14569ad2d0c35663015d01aab861488cc
dynamasoft <smithtanny@gmail.com>
2021-10-22 13:23:19 -0400

    some updates
    some updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,36 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button, Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import {
+  Button,
+  Container,
+  AppBar,
+  Typography,
+  Grow,
+  Grid,
+} from "@material-ui/core";
 import { ethers } from "ethers";
 import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
-import Contract from "./contract"
+import Contract from "./contract";
 
 function App() {
-
   const [logs, setLogs] = useState([]);
   const [propertyID, setPropertyID] = useState(0);
   const [applicationID, setApplicationID] = useState(0);
+  const [contract, setContract] = useState();
 
   useEffect(() => {
-    test();
-  },[]);
+    setContract(new Contract());
+    displayMessage("Contract instantiated.");
+  }, []);
 
-  function displayMessage(msg) {    
+  function displayMessage(msg) {
     setLogs((logs) => [...logs, msg]);
   }
 
-  async function test()
-  {    
-    var contract = new Contract();
-    displayMessage("Contract instantiated.");
+  const start = async () => {
     await contract.initialize();
-    displayMessage("Contract initialized.");    
+    displayMessage("Contract initialized.");
     var propertyID = await contract.listProperty("123 main st", 1500, 2);
     displayMessage("property listed.");
     setPropertyID(propertyID);
@@ -34,23 +39,34 @@ function App() {
     displayMessage("Approved property listing.");
     debugger;
     var applicationID = await contract.applyToRental(propertyID, 1500);
-    displayMessage("Applicant apply to property : " + propertyID + " with applicationid " + applicationID);
+    displayMessage(
+      "Applicant apply to property : " +
+        propertyID +
+        " with applicationid " +
+        applicationID
+    );
     setApplicationID(applicationID);
     await contract.declineApplicant(applicationID);
-    displayMessage("Owner declined applicant with applicationID " + applicationID);
-    await contract.startRentalProcess(applicationID, applicationApproved, applicationRejected);  
-    displayMessage("Property owner started the rental process...wait for the result below from oracles");
+    displayMessage(
+      "Owner declined applicant with applicationID " + applicationID
+    );
+    await contract.startRentalProcess(
+      applicationID,
+      applicationApproved,
+      applicationRejected
+    );
+    displayMessage(
+      "Property owner started the rental process...wait for the result below from oracles"
+    );
     displayMessage("waiting.......its coming....wait......");
+  };
+
+  async function applicationApproved(applicationID) {
+    displayMessage("application : " + applicationID + " has been approved.");
   }
 
-  async function applicationApproved(applicationID)
-  {
-    displayMessage("application : " + applicationID + " has been approved.")
-  }
-
-  async function applicationRejected(applicationID)
-  {
-    displayMessage("application : " + applicationID + " has been rejected.")
+  async function applicationRejected(applicationID) {
+    displayMessage("application : " + applicationID + " has been rejected.");
   }
 
   var property1 = require("./images/property_1.jpg").default;
@@ -58,7 +74,7 @@ function App() {
   var property3 = require("./images/property_3.jpg").default;
 
   return (
-    <div className="App">      
+    <div className="App">
       <main>
         <Grid container>
           <Grid item xs={12}>
@@ -71,45 +87,68 @@ function App() {
                   style={{ maxWidth: "300px", maxHeight: "500px" }}
                 />
                 <p align="center">
-                  is simply dummy text of the printing and typesetting industry.
-                  Lorem Ipsum has been the industry's standard dummy text ever
-                  since the 1500s, when an unknown printer took a galley of type
-                  and scrambled it to make a type specimen book. It has survived               
+                  I have a Master Bedroom for rent in a townhome, you will have
+                  your own bedroom and bathroom with walk in closet, garden tub,
+                  and shower. The kitchen, living room, and washer/dryer is
+                  shared. Close to Brandon Mall, I-, and many restaurants. This
+                  is a safe family gated community with a pool.
                 </p>
-                <Button color="primary" variant="contained">Apply</Button>
+                <Button color="primary" variant="contained" onClick={() => start()}> 
+                  Apply
+                </Button>
               </Grid>
               <Grid item xs>
-                <img src={property2} style={{ maxWidth: "300px", maxHeight: "500px" }}/>
+                <img
+                  src={property2}
+                  style={{ maxWidth: "300px", maxHeight: "500px" }}
+                />
                 <p align="center">
-                  is simply dummy text of the printing and typesetting industry.
-                  Lorem Ipsum has been the industry's standard dummy text ever
-                  since the 1500s, when an unknown printer took a galley of type
-                  and scrambled it to make a type specimen book. It has survived               
+                  Looking for a new roommate. A stunning NEWLY RENOVATED
+                  Victorian terrace with an abundance of CHARACTER FEATURES,
+                  located within the catchment area of EXCELLENT SCHOOLS and
+                  benefiting from a wealth of local amenities. It would make the
+                  perfect FAMIILY HOME.
                 </p>
-                <Button color="primary" variant="contained">Apply</Button>
+                <Button color="primary" variant="contained" onClick={() => start()}>
+                  Apply
+                </Button>
               </Grid>
               <Grid item xs>
-                <img src={property3} style={{ maxWidth: "300px", maxHeight: "500px" }}/>
+                <img
+                  src={property3}
+                  style={{ maxWidth: "300px", maxHeight: "500px" }}
+                />
                 <p align="center">
-                  is simply dummy text of the printing and typesetting industry.
-                  Lorem Ipsum has been the industry's standard dummy text ever
-                  since the 1500s, when an unknown printer took a galley of type
-                  and scrambled it to make a type specimen book. It has survived               
+                  Room for Rent in Beautiful & Safe West San Jose'. Room is
+                  fully furnished~for one person, only. Looking for one person
+                  who is very quiet, clean, neat, responsible & respectful. Home
+                  is Chemical & Scent free & Environmentally healthy &
+                  conscious. Rent is: $, per month with security deposit of
+                  same. $, non refundable cleaning fee is required. Must have
+                  work/personal & previous rental references. Close to tech,
+                  hospitals, shopping & freeways. Walk to Santana Row &
+                  Winchester Mystery House. Park in driveway.
                 </p>
-                <Button color="primary" variant="contained">Apply</Button>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  onClick={() => start()}
+                >
+                  Apply
+                </Button>
               </Grid>
             </Grid>
-          </Grid>          
-          <Grid item xs={12} style={{marginTop: "100px"}}>            
-        <h2> INTERNAL PROCESS BEHIND THE SCENE</h2>
-          {logs.map((log) => (
-          <div>{log}</div>
-          ))}
           </Grid>
-        </Grid>       
+          <Grid item xs={12} style={{ marginTop: "100px" }}>
+            <h2> INTERNAL PROCESS BEHIND THE SCENE</h2>
+            {logs.map((log) => (
+              <div>{log}</div>
+            ))}
+          </Grid>
+        </Grid>
       </main>
     </div>
-  )
+  );
 }
 
 export default App;
